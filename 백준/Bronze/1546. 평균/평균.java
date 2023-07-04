697f580379c6cc4987838ab07195c4a50a6f8d6d
Euncheol Kim <72078208+euncheol-kim@users.noreply.github.com>
2023-07-04 20:21:12 +0900

    [Bronze I] Title: 평균, Time: 136 ms, Memory: 14632 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,32 +1,24 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine(); // 첫 라인에 들어가는 숫자 :: 쓰지 않음
-        String[] strNums = br.readLine().split(" ");
 
-        // doulb[] 배열 저장
-        double[] originNums = Arrays.stream(strNums)
-                .mapToDouble(Double::parseDouble)
+        int subjects = Integer.parseInt(br.readLine());
+        int[] scores = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
                 .toArray();
+        int great = Arrays.stream(scores).max().getAsInt();
 
-        // stream descending sorted를 위한 stream.boxed
-        Double[] descNums = Arrays.stream(originNums)
-                .boxed()
-                .sorted(Comparator.reverseOrder())
-                .toArray(Double[]::new);
-
-        double topScore = descNums[0];
-        for(int i = 0; i < descNums.length; i++) {
-            // 점수 / 최고점 * 100
-            originNums[i] = (descNums[i] / topScore) * 100;
+        double result = 0;
+        for (int i = 0; i < scores.length; i++) {
+            double convertScore = (double) scores[i] / great * 100;
+            result += convertScore;
         }
 
-        double result = Arrays.stream(originNums).average().getAsDouble();
-        System.out.println(result);
+        System.out.println(result / subjects);
     }
 }
