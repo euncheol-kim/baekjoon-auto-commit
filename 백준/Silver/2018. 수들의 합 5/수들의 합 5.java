a7df8a6b6013490343df37a19d54d9be6da5316f
Euncheol Kim <72078208+euncheol-kim@users.noreply.github.com>
2023-07-10 16:44:39 +0900

    [Silver V] Title: 수들의 합 5, Time: 220 ms, Memory: 53784 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -4,40 +4,35 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Integer input = Integer.parseInt(br.readLine());
+        Integer n = Integer.parseInt(br.readLine());
+        int result = 1;
+        int sum = 0;
+        int start = 0;
+        int end = 0;
 
-        int[] nums = new int[input + 1];
+        int[] nums = new int[n + 1];
 
         // 순차적 숫자의 초기화 진행
-        for(int i = nums.length - 1; i > 0; i--) {
+        for (int i = 1; i < nums.length; i++) {
             nums[i] = i;
         }
 
-        int condition = nums.length - 1;
-        int result = 0;
-        while(condition > 0) {
-            result += calculator(input, nums, condition);
-            condition --;
+        // query
+        while(n != end) {
+            if(sum == n) {
+                result ++;
+                end ++;
+                sum += nums[end];
+            } else if(sum < n){
+                end ++;
+                sum += nums[end];
+            } else  {
+                sum = sum - nums[start];
+                start ++;
+            }
         }
 
         System.out.println(result);
-    }
-
-    private static int calculator(int input, int[] nums, int condition) {
-        // 첫 번째 들어오는 수는 반드시 카운트한다.
-        if(nums[condition] == input) {
-            return 1;
-        } else {
-            // input값과 연속된 숫자의 합인지 판별하는 코드
-            int sum = 0;
-            do {
-                sum = sum + nums[condition];
-                condition --;
-            } while(sum < input && condition >= 1);
-
-            if(sum == input) return 1;
-            return 0;
-        }
     }
 
 }
