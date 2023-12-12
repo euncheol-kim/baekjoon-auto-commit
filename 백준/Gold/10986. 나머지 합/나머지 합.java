a1bb544256906c82f830742802cfcc371e9c88a6
Euncheol Kim <72078208+euncheol-kim@users.noreply.github.com>
2023-12-12 19:47:01 +0900

    [Gold III] Title: 나머지 합, Time: 544 ms, Memory: 146044 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,35 +7,24 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] NM = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int N = NM[0];
+        int M = NM[1];
+
+        long[] nums = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+        long[] prefixSums = new long[nums.length + 1];
+        long[] prefixSumsR = new long[M];
+        for(int i = 1; i < prefixSums.length; i++) {
+            prefixSums[i] = nums[i-1] + prefixSums[i-1];
+            long r = prefixSums[i] % M;
+            prefixSumsR[(int)r] += 1;
+        }
+
         long result = 0;
-
-        int[] NM = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        int n = NM[0];
-        int m = NM[1];
-
-        long[] nums = Arrays.stream(br.readLine().split(" "))
-                .mapToLong(Long::parseLong)
-                .toArray();
-
-        long[] prefixSumModM = new long[nums.length + 1];
-        long[] remainders = new long[m];
-        for(int i = 1; i < prefixSumModM.length; i++) {
-            prefixSumModM[i] = (prefixSumModM[i - 1] + nums[i - 1]) % m;
-
-            if (prefixSumModM[i] == 0) result ++;
-            remainders[(int)prefixSumModM[i]] ++;
+        for(int i = 0; i < prefixSumsR.length; i++) {
+            result = result + ( (prefixSumsR[i] * (prefixSumsR[i] - 1) ) / 2 );
         }
 
-        for(int i = 0; i < remainders.length; i++) {
-            if(remainders[i] > 1)  {
-                result = result + ( (remainders[i] * (remainders[i] - 1) ) / 2 );
-            }
-        }
-
-        System.out.print(result);
-
+        System.out.println(result + prefixSumsR[0]);
     }
 }
