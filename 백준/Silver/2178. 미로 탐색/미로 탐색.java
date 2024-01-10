53d7235b263abc53f033ded6960f64c4f54dba09
Euncheol Kim <72078208+euncheol-kim@users.noreply.github.com>
2024-01-10 19:58:26 +0900

    [Silver I] Title: 미로 탐색, Time: 200 ms, Memory: 16936 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,89 +1,103 @@
+
 import java.util.*;
 import java.io.*;
 
+// 더러운코드
+// 더러운코드
+// 더러운코드
+// 더러운코드
+// 더러운코드
+// 더러운코드
+
+
+
 public class Main {
-    public static int n;
-    public static int m;
 
-    // operationBFS; BFS연산값 저장
-    public static int[][] operationMatrix;
-
-    // 배열 기준 -> (상, 하, 좌, 우)
-    public static int[] dx = {0, 0 , -1, 1};
+    public static int[] dx = {0, 0, -1, 1};
     public static int[] dy = {-1, 1, 0, 0};
+
+    public static class XY {
+        public int x;
+        public int y;
+
+        public XY(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getX() {
+            return this.x;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public int getY() {
+            return this.y;
+        }
+    }
+
+    public static int getMinimumDistance(int[][] matrix, boolean[][] visited, Queue<XY> queue,
+                                         int N, int M, int depth) {
+        visited[N - 1][M - 1] = true;
+        queue.add(new XY(N - 1, M - 1));
+
+        while (!queue.isEmpty()) {
+            XY xy = queue.remove();
+
+            for (int i = 0; i < 4; i++) {
+                int nX = xy.getX() + dx[i];
+                int nY = xy.getY() + dy[i];
+
+                // nx와 ny가 matrix범위 넘으면 continue
+                if (nX < 0 || N <= nX || nY < 0 || M <= nY) {
+                    continue;
+                }
+
+                // 방문한 노드라면 또는 0인값이라면
+                if (visited[nX][nY] || matrix[nX][nY] == 0) {
+                    continue;
+                }
+
+                visited[nX][nY] = true;
+                matrix[nX][nY] = matrix[nX][nY] + matrix[nX - dx[i]][nY - dy[i]];
+                depth = depth + 1;
+                queue.add(new XY(nX, nY));
+
+                if (visited[0][0]) return matrix[0][0];
+            }
+        }
+
+        return matrix[0][0];
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // input 1Line
-        int[] nm = Arrays.stream(br.readLine().split(" "))
+        int[] NM = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
-        n = nm[0];
-        m = nm[1];
+        int N = NM[0];
+        int M = NM[1];
 
-        // matrix && visited && operationBFS 생성
-        int[][] matrix = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
-
-        // 사용자 데이터 값 입력 받기 및 matrix 값 저장 [ Input -> 2Line ~ last Line]
-        for(int i = 0; i < n; i++) {
-            int[] line = Arrays.stream(br.readLine().split(""))
+        int[][] matrix = new int [N][M];
+        boolean[][] visited = new boolean [N][M];
+        for(int i=0; i < matrix.length; i++) {
+            int[] temp = Arrays.stream(br.readLine().split(""))
                     .mapToInt(Integer::parseInt)
                     .toArray();
 
-            for(int j = 0; j < m; j++) {
-                matrix[i][j] = line[j];
-            }
+            matrix[i] = temp;
         }
 
-        operatedBFS(matrix, visited);
-        System.out.println(operationMatrix[n - 1][m - 1]);
-    }
+        Queue<XY> queue = new LinkedList<>();
+        int depth = getMinimumDistance(matrix, visited, queue, N, M, 1);
+        System.out.println(depth);
 
-    public static void operatedBFS(int[][] matrix, boolean[][] visited) {
-        operationMatrix = new int[n][m];
-
-        // operationMatrix; 연산에 사용할 2차원 int 배열 선언
-        for(int i = 0; i < n; i++) {
-            System.arraycopy(matrix[i],0, operationMatrix[i], 0, operationMatrix[i].length - 1);
-        }
-
-        operatedBFS(matrix, visited, operationMatrix);
-    }
-
-    public static void operatedBFS(int[][] matrix,
-                                  boolean[][] visited,
-                                  int[][] operationMatrix) {
-
-        // queue; 연산에 이용할 queue
-        Queue<int[]> queue = new LinkedList<>();
-        // 시작위치 삽입
-        queue.offer(new int[]{0, 0});
-        visited[0][0] = true;
-
-        while( !queue.isEmpty() ) {
-            int[] cur = queue.poll();
-
-            for(int i = 0; i < 4; i ++) {
-                int x = cur[0] + dx[i];
-                int y = cur[1] + dy[i];
-
-                // matrix 범위를 넘어가지 않는 경우
-                if( x >= 0 && y >= 0 && x < n && y < m ) {
-
-                    // 0이 아닌 경우, 방문하지 않은 경우
-                    if( matrix[x][y] != 0 && !visited[x][y] ) {
-                        visited[x][y] = true;
-                        queue.offer(new int[]{x, y});
-
-                        operationMatrix[x][y] = operationMatrix[cur[0]][cur[1]] + 1 ;
-                    }
-
-                }
-
-            }
-        }
     }
 }
